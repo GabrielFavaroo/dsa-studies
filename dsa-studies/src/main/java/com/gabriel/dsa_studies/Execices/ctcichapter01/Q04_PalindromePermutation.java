eba28868c66665932815ada6738573a3d77a7b99
OrcadiusDev <gabriel14fav@gmail.com>
2026-07-03 21:27:53 -0300

    Questão 4

--- dsa-studies/src/main/java/com/gabriel/dsa_studies/Execices/ctcichapter01/Q04_PalindromePermutation.java
+++ dsa-studies/src/main/java/com/gabriel/dsa_studies/Execices/ctcichapter01/Q04_PalindromePermutation.java
@@ -1,4 +1,42 @@
 package com.gabriel.dsa_studies.Execices.ctcichapter01;
 
 public class Q04_PalindromePermutation {
+
+    public Boolean palindromePermutationWithHashmap (String word){
+
+        HashMap<Character,Integer> occurrences = new HashMap<>();
+
+        for(char c : word.toCharArray()){
+
+            if(!occurrences.containsKey(c)){
+                occurrences.put(c,1);
+            }
+            else{
+                occurrences.put(c,occurrences.getOrDefault(c,0)+1);
+            }
+
+        }
+
+        switch (word.length()){
+
+            case 0: for (Map.Entry<Character, Integer> map : occurrences.entrySet()) {
+                if(map.getValue() % 2 != 0){return false;}
+
+            }
+
+            case 1: for (Map.Entry<Character, Integer> map : occurrences.entrySet()) {
+                if(map.getValue() % 2 != 0){return true;}
+
+            }
+
+
+        }
+
+        
+
+        return false;
+
+    }
+
+
 }
